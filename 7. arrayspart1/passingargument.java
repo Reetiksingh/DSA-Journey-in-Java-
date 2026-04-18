f7c42d4238a9a97916fb5981bd52067cb26f9f99
Your Name <you@example.comclear>
2026-04-18 21:08:29 +0530

    Day 13- 16 arrats

---
+++ 7. arrayspart1/passingargument.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class passingargument {
+    public static void update(int marks[], int nonchangable) {
+        nonchangable = 10;
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = marks[i] + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int marks[] = { 97, 98, 99 };
+        int nonchangable = 5;
+        update(marks, 10);
+        System.out.println(nonchangable);
+
+        for (int i = 0; i < marks.length; i++) {
+            System.out.print(marks[i]+" ");
+        }
+        System.out.println();
+    }
+}
---
+++ 8. arrayspart2/maxsubarrsumbruteforce.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class maxsubarrsumbruteforce {
+    public static void maxsubarraysum(int numbers[]){
+        int currsum=0;
+        int maxsum=Integer.MIN_VALUE;
+
+        for(int i=0; i<numbers.length; i++){
+            int start=i;
+
+            for(int j=0;j<numbers.length; j++){
+                int end=j;
+                currsum=0;
+
+                for(int k=start; k<=end; k++){
+                    currsum+=numbers[k];
+            }
+            System.out.println(currsum);
+            if(maxsum<currsum){
+                maxsum=currsum;
+            }
+        }
+    }
+    System.out.println("Max sum= "+maxsum);
+
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = { 1, -2, 6, -1, 3 };
+        maxsubarraysum(numbers);
+    }
+
+}
